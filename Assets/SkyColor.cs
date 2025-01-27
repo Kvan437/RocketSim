6911e5d24ec47b86370dec46f5508e9ab2a3d682
Kvan437 <KennethVan437@gmail.com>
2025-01-26 20:54:59 -0600

    scene saved

---
+++ Assets/SkyColor.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class SkyColor : MonoBehaviour
+{
+
+    public GameObject rocket;
+
+    private Camera camera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        camera = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
