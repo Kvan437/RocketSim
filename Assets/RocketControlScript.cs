0d6056cdec38b57b14543db44e6e05a5e43237eb
Kvan437 <KennethVan437@gmail.com>
2025-01-26 20:42:17 -0600

    up

--- Assets/RocketControlScript.cs
+++ Assets/RocketControlScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RocketControlScript : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,9 @@ public class RocketControlScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity += new Vector2(0, 10);
+        }
     }
 }
